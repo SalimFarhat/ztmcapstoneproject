4ee0271891b9a55dbada7d4bfd2d1d1c2c2b4d87
Salim Farhat <salimfarhat@gmail.com>
2023-06-19 00:04:39 -0400

    repaired some issues.

--- src/components/form-input/form-input.styles.jsx
+++ src/components/form-input/form-input.styles.jsx
@@ -1,16 +1,25 @@
-import styled, {css} from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const subColor = 'grey';
 const mainColor = 'black';
 
-
-const shrinkLabel = css`
+const shrinkLabelStyles = css`
   top: -14px;
   font-size: 12px;
   color: ${mainColor};
-  
-`
+`;
 
+export const FormInputLabel = styled.label`
+  color: ${subColor};
+  font-size: 16px;
+  font-weight: normal;
+  position: absolute;
+  pointer-events: none;
+  left: 5px;
+  top: 10px;
+  transition: 300ms ease all;
+  ${({ shrink }) => shrink && shrinkLabelStyles};
+`;
 
 export const Input = styled.input`
   background: none;
@@ -30,30 +39,15 @@ export const Input = styled.input`
   }
 
   &:focus ~ ${FormInputLabel} {
-    ${shrinkLabel};
+    ${shrinkLabelStyles};
   }
-  
-`
+`;
 
-export const FormInputLabel = styled.label`
-  color: ${subColor};
-  font-size: 16px;
-  font-weight: normal;
-  position: absolute;
-  pointer-events: none;
-  left: 5px;
-  top: 10px;
-  transition: 300ms ease all;
-  
-  ${({shrink}) => shrink && shrinkLabel };
-
-`
 export const Group = styled.div`
   position: relative;
   margin: 45px 0;
+
   input[type='password'] {
     letter-spacing: 0.3em;
   }
-  
-`
-
+`;
